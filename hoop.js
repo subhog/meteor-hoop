e185e98914f01c1e3ebb5f2270571b8279d6eec0
Hubert OG <hubert@orlikarnia.com>
2013-04-22 13:57:44 +0200

    Small fix

--- hoop.js
+++ hoop.js
@@ -2,18 +2,18 @@ $extends = function(child, parent) {
   child.prototype = new parent;
   child.prototype.constructor = child;
   child.prototype.super = parent.prototype;
-}
+};
 
 $functions = function(f, object) {
   for(var key in object) {
     f.prototype[key] = object[key];   
   }
-}
+};
 
 $statics = function(f, object) {
   for(var key in object) {
     f[key] = object[key];   
   }
-}
+};
 
 
