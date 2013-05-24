08b059e091147d25930d4d17111c4b90fcbd1a47
Hubert OG <hubert@orlikarnia.com>
2013-05-24 10:39:51 +0200

    Fixed typo

--- package.js
+++ package.js
@@ -8,5 +8,5 @@ Package.on_use(function (api) {
 
   // api.add_files('roles_server.js', 'server');
   // api.add_files('roles_common.js', both);
-  api.add_files('hoop.js', ['client', 'server]');
+  api.add_files('hoop.js', ['client', 'server']);
 });
